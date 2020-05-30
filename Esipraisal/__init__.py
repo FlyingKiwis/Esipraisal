46b67e77a518a763e7c2edc1c7e2750bb76339b9
FlyingKiwis <kiwi@flyingkiwibird.com>
2020-05-30 12:58:01 -0400

    0.0.2

--- Esipraisal/__init__.py
+++ Esipraisal/__init__.py
@@ -5,5 +5,5 @@ except ImportError:
 
     pass
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __name__ = 'EsiPyMarket'
--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+### 0.0.2
+
+- Tidy up, change print to logging
+
 ### 0.0.1
 
 - Initial working version
